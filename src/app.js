5b7b20b6c231013bee2254d2baed26d08a6b6f84
raymart.libiran@ist-systems.com <raymart.libiran@ist-systems.com>
2026-05-08 23:25:40 +0800

    increase file size upload limit

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: '30mb' }));
+app.use(express.json({ limit: '500mb' }));
+app.use(express.urlencoded({ limit: '500mb', extended: true }));
 
 app.get('/health', (req, res) => {
     res.send('API is running');
